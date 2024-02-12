f85f0baed7ffa9bc575e43f839e00d55a525af86
taylor <taylor.laekeman@gmail.com>
2024-02-12 15:23:03 -0500

    add cdk boilerplate

---
+++ test/peanut-gallery-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PeanutGalleryCdk from '../lib/peanut-gallery-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/peanut-gallery-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PeanutGalleryCdk.PeanutGalleryCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
