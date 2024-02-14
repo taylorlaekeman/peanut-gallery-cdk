378ffd850c05136770efb50b9e30aaa9ff374fe5
taylor <taylor.laekeman@gmail.com>
2024-02-14 11:28:30 -0500

    add dynamo table to cdk stack

--- lib/peanut-gallery-cdk-stack.ts
+++ lib/peanut-gallery-cdk-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PeanutGalleryCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PeanutGalleryCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, 'MoviesTable', {
+      partitionKey: { name: 'year-week', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'score', type: dynamodb.AttributeType.NUMBER },
+    })
   }
 }
