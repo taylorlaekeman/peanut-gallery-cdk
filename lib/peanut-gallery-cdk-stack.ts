9347df7c1544c7d1788d3e45a7be3c08d2796f9a
taylor <taylor.laekeman@gmail.com>
2024-03-13 13:20:34 -0400

    create moviesByScore secondary index and update table key to use id to guarantee uniqueness

--- lib/peanut-gallery-cdk-stack.ts
+++ lib/peanut-gallery-cdk-stack.ts
@@ -1,14 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 
 export class PeanutGalleryCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.Table(this, 'MoviesTable', {
-      partitionKey: { name: 'year-week', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'score', type: dynamodb.AttributeType.NUMBER },
-    })
+    const table = new dynamodb.TableV2(this, "MoviesTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      globalSecondaryIndexes: [
+        {
+          indexName: "moviesByScore",
+          partitionKey: {
+            name: "year-week",
+            type: dynamodb.AttributeType.STRING,
+          },
+          sortKey: { name: "score-id", type: dynamodb.AttributeType.STRING },
+        },
+      ],
+    });
   }
 }
