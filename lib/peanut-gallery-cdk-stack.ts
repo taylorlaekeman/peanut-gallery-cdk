cf749c04fcd7ce910b525e4ac53e1a3f7135440e
taylor <taylor.laekeman@gmail.com>
2024-03-25 15:16:38 -0400

    add graphql lambda

--- lib/peanut-gallery-cdk-stack.ts
+++ lib/peanut-gallery-cdk-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class PeanutGalleryCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.TableV2(this, "MoviesTable", {
+    const table = new dynamodb.TableV2(this, getName("MoviesTable"), {
       globalSecondaryIndexes: [
         {
           indexName: "moviesByScore",
@@ -18,7 +20,22 @@ export class PeanutGalleryCdkStack extends cdk.Stack {
         },
       ],
       partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
-      tableName: 'MoviesTable',
+      tableName: "MoviesTable",
+    });
+
+    const graphqlLambdaS3 = new s3.Bucket(this, getName("GraphQLLambdaBucket"));
+
+    const graphqlLambda = new lambda.Function(this, getName("GraphQLLambda"), {
+      code: lambda.Code.fromBucket(
+        graphqlLambdaS3,
+        getName("GraphQLLambdaCode")
+      ),
+      handler: 'index.main',
+      runtime: lambda.Runtime.NODEJS_18_X,
     });
   }
+}
+
+function getName(name: string): string {
+  return `PeanutGallery${name}`;
 }
