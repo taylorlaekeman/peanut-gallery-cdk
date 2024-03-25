9dd6b4e4c1e6714fbbacad03c999e3eb887277a0
taylor <taylor.laekeman@gmail.com>
2024-03-25 16:46:45 -0400

    change dist.index to dist/index in handler

--- lib/peanut-gallery-cdk-stack.ts
+++ lib/peanut-gallery-cdk-stack.ts
@@ -32,7 +32,7 @@ export class PeanutGalleryCdkStack extends cdk.Stack {
     const graphqlLambda = new lambda.Function(this, getName("GraphQLLambda"), {
       code: lambda.Code.fromInline(DEFAULT_HANDLER_CODE),
       functionName: getName("GraphQLLambda"),
-      handler: "dist.index.handler",
+      handler: "dist/index.handler",
       runtime: lambda.Runtime.NODEJS_18_X,
     });
   }
